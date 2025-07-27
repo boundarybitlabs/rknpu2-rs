761c45d7b334cb1b6ce167a0d47fb3ef89a78f75
Boyd Johnson <boyd@boundarybit.io>
2025-07-27 12:31:24 +0000

    Add PerfRun struct for outputting the duration

--- crates/rknpu2/src/query.rs
+++ crates/rknpu2/src/query.rs
@@ -11,7 +11,13 @@ pub mod input_attr;
 pub mod output_attr;
 pub mod sdk_version;
 
+#[cfg(any(feature = "rk35xx", feature = "rk3576"))]
+pub mod perf_run;
+
 pub use {in_out_num::InputOutputNum, sdk_version::SdkVersion};
+
+#[cfg(any(feature = "rk35xx", feature = "rk3576"))]
+pub use perf_run::PerfRun;
 
 pub trait QueryWithInput: From<Self::Output> + Sized {
     const QUERY_TYPE: Type;
@@ -23,4 +29,4 @@ pub trait QueryWithInput: From<Self::Output> + Sized {
     fn prepare(input: Self::Input, output: &mut Self::Output);
 }
 
-pub use input_attr::InputAttr;
+pub use {input_attr::InputAttr, output_attr::OutputAttr};
--- crates/rknpu2/tests/test_run.rs
+++ crates/rknpu2/tests/test_run.rs
@@ -18,3 +18,25 @@ fn test_run() {
     let output = outputs[0].as_slice();
     assert_eq!(output.len(), 1000);
 }
+
+#[cfg(any(feature = "rk3576", feature = "rk35xx"))]
+#[test]
+fn test_perf_run() {
+    use rknpu2::query::PerfRun;
+
+    let mut model_data = MODEL_DATA.to_vec();
+    let model = RKNN::new(&mut model_data, 0).unwrap();
+
+    let mut input = TensorBuilder::new_input(&model, 0)
+        .allocate::<i8>()
+        .unwrap();
+    input.fill_with(0i8);
+    model.set_inputs(&[input]).unwrap();
+    model.run().unwrap();
+    let outputs = model.get_outputs::<i8>().unwrap();
+    let output = outputs[0].as_slice();
+    assert_eq!(output.len(), 1000);
+
+    let perf_run = model.query::<PerfRun>().unwrap();
+    assert!(perf_run.run_duration() > 0);
+}
