9cf0aca1053503ad944db928e66001388d0d01c2
Boyd Johnson <boyd@boundarybit.io>
2025-08-06 20:39:33 +0000

    Add nc1hwc2 input,output query types

--- crates/rknpu2/src/query.rs
+++ crates/rknpu2/src/query.rs
@@ -10,6 +10,8 @@ pub trait Query: From<Self::Output> + Sized {
 pub mod in_out_num;
 pub mod input_attr;
 pub mod native_input_attr;
+pub mod native_nc1hwc2_input_attr;
+pub mod native_nc1hwc2_output_attr;
 pub mod native_nhwc_input_attr;
 pub mod native_nhwc_output_attr;
 pub mod native_output_attr;
@@ -32,6 +34,8 @@ pub mod perf_detail;
 
 pub use {
     in_out_num::InputOutputNum, native_input_attr::NativeInputAttr,
+    native_nc1hwc2_input_attr::NativeNC1HWC2InputAttr,
+    native_nc1hwc2_output_attr::NativeNC1HWC2OutputAttr,
     native_nhwc_input_attr::NativeNHWCInputAttr, native_nhwc_output_attr::NativeNHWCOutputAttr,
     native_output_attr::NativeOutputAttr, sdk_version::SdkVersion,
 };
